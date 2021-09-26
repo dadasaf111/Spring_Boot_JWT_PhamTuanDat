8b066371324802a489309aafbc9dfa4a57b196bd
dadasaf111 <vanteonguyen955@gmail.com>
2021-09-26 16:06:46 +0700

    add login and generateToken

--- src/main/java/com/example/spring_boot_jwt_phamtuandat/util/JwtUtil.java
+++ src/main/java/com/example/spring_boot_jwt_phamtuandat/util/JwtUtil.java
@@ -17,8 +17,8 @@ import java.util.Date;
 public class JwtUtil {
 
     private static Logger logger = LoggerFactory.getLogger(JwtUtil.class);
-    private static final String USER = "tien";
-    private static final String SECRET = "hey Mr Tien the secrect length must be at least 256 bits" +
+    private static final String USER = "dat";
+    private static final String SECRET = "hey Mr Dat the secrect length must be at least 256 bits" +
             " please no reveal!";
 
     public String generateToken(UserPrincipal user) {
